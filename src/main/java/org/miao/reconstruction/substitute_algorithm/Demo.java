7df52b0aa56a4287b1d58e5ccce09cbccb10e52e
miao1564293969 <miao1564293969@.github.com>
2020-04-10 13:20:18 +0800

    替换算法-原始函数

---
+++ src/main/java/org/miao/reconstruction/substitute_algorithm/Demo.java
@@ -0,0 +1,18 @@
+package org.miao.reconstruction.substitute_algorithm;
+
+public class Demo {
+    String foundPerson(String[] people) {
+        for(int i = 0; i < people.length; i++){
+            if(people[i].equals("Don")){
+                return "Don";
+            }
+            if(people[i].equals("John")){
+                return "John";
+            }
+            if(people[i].equals("Kent")){
+                return "Kent";
+            }
+        }
+        return "";
+    }
+}
