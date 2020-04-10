0933d4119f0e39ec1d6aef00c2b5d0881798e2f1
miao1564293969 <miao1564293969@.github.com>
2020-04-10 10:41:18 +0800

    以函数对象取代函数-原始函数

--- src/main/java/org/miao/reconstruction/remove_assign_to_param/Demo.java
+++ src/main/java/org/miao/reconstruction/remove_assign_to_param/Demo.java
@@ -2,9 +2,10 @@ package org.miao.reconstruction.remove_assign_to_param;
 
 public class Demo {
     int discount(int inputVal, int quantity, int yearToDate){
-        if (inputVal > 50 ) inputVal -= 2;
-        if (quantity > 100 ) inputVal -= 1;
-        if (yearToDate > 10000) inputVal -= 4;
-        return inputVal;
+        int result = inputVal;
+        if (inputVal > 50 ) result -= 2;
+        if (quantity > 100 ) result -= 1;
+        if (yearToDate > 10000) result -= 4;
+        return result;
     }
 }
---
+++ src/main/java/org/miao/reconstruction/replace_method_with_method_object/Account.java
@@ -0,0 +1,15 @@
+package org.miao.reconstruction.replace_method_with_method_object;
+
+public class Account {
+    int gamma (int inputVal, int quantity,int yearToDate) {
+        int importantValue1 = (inputVal*quantity) + delta();
+        int importantValue2 = (inputVal*yearToDate) + 100;
+        if (yearToDate - importantValue1 > 100)
+            importantValue2 -= 20;
+        int importantValue3 = importantValue2 * 7;
+        return importantValue3 - 2 * importantValue1;
+    }
+    int delta(){
+        return 0;
+    }
+}
