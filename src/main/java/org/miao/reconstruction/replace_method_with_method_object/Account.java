ee75505a2c8d11c135240e453bf6074c2e292f44
miao1564293969 <miao1564293969@.github.com>
2020-04-10 13:12:21 +0800

    以函数对象取代函数-改进函数

--- src/main/java/org/miao/reconstruction/replace_method_with_method_object/Account.java
+++ src/main/java/org/miao/reconstruction/replace_method_with_method_object/Account.java
@@ -2,14 +2,7 @@ package org.miao.reconstruction.replace_method_with_method_object;
 
 public class Account {
     int gamma (int inputVal, int quantity,int yearToDate) {
-        int importantValue1 = (inputVal*quantity) + delta();
-        int importantValue2 = (inputVal*yearToDate) + 100;
-        if (yearToDate - importantValue1 > 100)
-            importantValue2 -= 20;
-        int importantValue3 = importantValue2 * 7;
-        return importantValue3 - 2 * importantValue1;
+        return new Gamma(this,inputVal,quantity,yearToDate).compute();
     }
-    int delta(){
-        return 0;
-    }
+
 }
---
+++ src/main/java/org/miao/reconstruction/replace_method_with_method_object/Gamma.java
@@ -0,0 +1,35 @@
+package org.miao.reconstruction.replace_method_with_method_object;
+
+public class Gamma {
+    private Account account;
+    private int importantValue1;
+    private int importantValue2;
+    private int importantValue3;
+    private int inputVal;
+    private int quantity;
+    private int yearToDate;
+
+    Gamma(Account account,int input, int quantity, int yearToDate){
+        this.account = account;
+        this.inputVal = input;
+        this.quantity = quantity;
+        this.yearToDate = yearToDate;
+    }
+
+    int compute() {
+        importantValue1 = (inputVal*quantity) + delta();
+        importantValue2 = (inputVal*yearToDate) + 100;
+        importantThing();
+        int importantValue3 = importantValue2 * 7;
+        return importantValue3 - 2 * importantValue1;
+    }
+
+    int delta(){
+        return 0;
+    }
+
+    void importantThing() {
+        if (yearToDate - importantValue1 > 100)
+            importantValue2 -= 20;
+    }
+}
