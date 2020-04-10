ad40ab38df84a463e978471369b82d31e1da76c4
miao1564293969 <miao1564293969@.github.com>
2020-04-10 09:49:58 +0800

    内联函数-改进函数

--- src/main/java/org/miao/reconstruction/inlinemethod/Demo.java
+++ src/main/java/org/miao/reconstruction/inlinemethod/Demo.java
@@ -3,9 +3,6 @@ package org.miao.reconstruction.inlinemethod;
 public class Demo {
     private int numberOfLateDeliveries;
     int getRating() {
-        return (moreThanFiveLateDeliveries()) ? 2 : 1;
-    }
-    boolean moreThanFiveLateDeliveries(){
-        return numberOfLateDeliveries > 5;
+        return (numberOfLateDeliveries > 5) ? 2 : 1;
     }
 }
