2bac9f72742694e15bed93b4620042afd06ac0bf
miao1564293969 <miao1564293969@.github.com>
2020-04-10 10:01:09 +0800

    以查询取代临时变量-改进函数

--- src/main/java/org/miao/reconstruction/replace_temp_with_query/Demo.java
+++ src/main/java/org/miao/reconstruction/replace_temp_with_query/Demo.java
@@ -4,10 +4,15 @@ public class Demo {
     private int quantity;
     private int itemPrice;
     double getPrice() {
-        int basePrice = quantity * itemPrice;
-        double discountFactor;
-        if (basePrice > 1000) discountFactor =0.95;
-        else discountFactor = 0.98;
-        return basePrice * discountFactor;
+        return basePrice() * discountFactor();
+    }
+
+    private int basePrice(){
+        return quantity * itemPrice;
+    }
+
+    private double discountFactor(){
+        if (basePrice() > 1000) return 0.95;
+        return 0.98;
     }
 }
